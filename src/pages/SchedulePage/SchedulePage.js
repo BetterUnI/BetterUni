12e06a1de6356a165ff6a92e0dacc8c10c76bf40
Davis Samuel <dsamuel5733@gmail.com>
2020-03-23 22:28:16 -0400

    Moved calendar to Profile page

--- src/pages/SchedulePage/SchedulePage.js
+++ src/pages/SchedulePage/SchedulePage.js
@@ -1,13 +1,10 @@
 import React from "react";
 
-import { BetterCalendar } from "../../components/Calendar/Calendar";
-
 export function SchedulePage() {
   return (
     <>
       <h1>Schedule page</h1>
       <p>This is the schedule page</p>
-      <BetterCalendar />
     </>
   );
 }
