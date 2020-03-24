490f8df894175b00edc264a822a31e704f2b5a07
Dillon Coffman <tug40394@temple.edu>
2020-03-24 15:21:33 -0400

    Fixed function error when parsing user uid from Cognito to integer

--- amplify/backend/function/betteruni6666fc7aPostConfirmation/src/custom.js
+++ amplify/backend/function/betteruni6666fc7aPostConfirmation/src/custom.js
@@ -5,8 +5,9 @@ console.log("starting post confirmation function");
 exports.handler = (event, context, callback) => {
   console.log("the postConfirmation user event request: ", event.request);
   let params = {};
+
   if (event.request.userAttributes.email && event.request.userAttributes.sub) {
-    const userParsedSub = parseInt(event.request.userAttributes.sub);
+    const userParsedSub = parseInt(1 + event.request.userAttributes.sub);
     const userUniqueId = userParsedSub * Math.floor(Math.random() * 100000 + 1);
     params = {
       Item: {
@@ -15,14 +16,14 @@ exports.handler = (event, context, callback) => {
       },
       TableName: "Users-dillon" // this value would stay hard coded, we can change it to the Users-master table when ready for deploy
     };
-  }
 
-  docClient.put(params, function(err, data) {
-    if (err) {
-      context.done(err);
-    } else {
-      console.log("saved user to dynamo db on post confirmation", data);
-      context.done(null, event);
-    }
-  });
+    docClient.put(params, function(err, data) {
+      if (err) {
+        callback(err, null);
+      } else {
+        console.log("saved user to dynamo db on post confirmation", data);
+        callback(null, event);
+      }
+    });
+  }
 };
