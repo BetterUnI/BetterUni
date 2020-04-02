76db9f84a9d6a4f9e334103b8cd83ddadf1cbfd9
Dillon Coffman <tug40394@temple.edu>
2020-04-02 18:36:57 -0400

    Updated parameters.json so client can read custom Cognito user attributes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./theme";
@@ -7,9 +7,19 @@ import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import Routes from "./Routes";
 
+import { Auth } from "aws-amplify";
+
 const browserHistory = createBrowserHistory();
 
 export function App(props) {
+  useEffect(() => {
+    // check the current user when the App component is loaded
+    Auth.currentAuthenticatedUser({ bypassCache: true })
+      .then(user => Auth.userAttributes(user))
+      .then(attributes => console.log(attributes))
+      .catch(err => console.log(err));
+  }, []);
+
   if (props.authState === "signedIn") {
     return (
       <ThemeProvider theme={theme}>
