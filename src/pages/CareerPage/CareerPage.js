760634507bffe3d43e17c502dd450654d7250568
tug67518 <tug67518@temple.edu>
2020-04-02 15:03:48 -0400

    Added career resource table layout

--- src/pages/CareerPage/CareerPage.js
+++ src/pages/CareerPage/CareerPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import CareerResourceList from "../../components/CareerResourceList/CareerResourceList";
+
 export function CareerPage() {
   return (
     <>
       <h1>Career page</h1>
       <p>This is the Career page</p>
+      <CareerResourceList />
     </>
   );
 }
