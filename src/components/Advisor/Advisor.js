2b2f1173755962d572c404f83afdd03e8099d8bb
Ariela Pellumbi <tuh40350@temple.edu>
2020-04-03 19:42:37 -0400

    Separated Category component that makes up CategoryList

--- src/components/Advisor/Advisor.js
+++ src/components/Advisor/Advisor.js
@@ -1,4 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/styles";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import { borders } from "@material-ui/system";
+import Box from "@material-ui/core/Box";
 
 export default function Advisor(props) {
   return <div className="advisor"></div>;
