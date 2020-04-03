bf4d19c331d604a776710934c770be5d7ece6478
tug67518 <tug67518@temple.edu>
2020-04-02 21:39:24 -0400

    Added career resource and upcoming career events data

--- src/components/CareerResourceList/CareerResourceList.js
+++ src/components/CareerResourceList/CareerResourceList.js
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import moment from "moment";
+import { Link } from "react-router-dom";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import { makeStyles } from "@material-ui/styles";
 import {
   Card,
   CardActions,
   CardContent,
-  Avatar,
-  Checkbox,
   Table,
-  TableBody,
   TableCell,
   TableHead,
-  TableRow,
-  Typography,
-  TablePagination
+  TableRow
 } from "@material-ui/core";
-
-import getInitials from "../../helpers/getInitials";
 
 const useStyles = makeStyles(theme => ({
   root: {},
@@ -46,52 +40,6 @@ const CareerResourceList = props => {
 
   const classes = useStyles();
 
-  const [selectedUsers, setSelectedUsers] = useState([]);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [page, setPage] = useState(0);
-
-  const handleSelectAll = event => {
-    const { users } = props;
-
-    let selectedUsers;
-
-    if (event.target.checked) {
-      selectedUsers = users.map(user => user.id);
-    } else {
-      selectedUsers = [];
-    }
-
-    setSelectedUsers(selectedUsers);
-  };
-
-  const handleSelectOne = (event, id) => {
-    const selectedIndex = selectedUsers.indexOf(id);
-    let newSelectedUsers = [];
-
-    if (selectedIndex === -1) {
-      newSelectedUsers = newSelectedUsers.concat(selectedUsers, id);
-    } else if (selectedIndex === 0) {
-      newSelectedUsers = newSelectedUsers.concat(selectedUsers.slice(1));
-    } else if (selectedIndex === selectedUsers.length - 1) {
-      newSelectedUsers = newSelectedUsers.concat(selectedUsers.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelectedUsers = newSelectedUsers.concat(
-        selectedUsers.slice(0, selectedIndex),
-        selectedUsers.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelectedUsers(newSelectedUsers);
-  };
-
-  const handlePageChange = (event, page) => {
-    setPage(page);
-  };
-
-  const handleRowsPerPageChange = event => {
-    setRowsPerPage(event.target.value);
-  };
-
   return (
     <Card {...rest} className={clsx(classes.root, className)}>
       <CardContent className={classes.content}>
@@ -105,7 +53,11 @@ const CareerResourceList = props => {
                   <TableCell>Link</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell>Handshake</TableCell>
+                  <TableCell>
+                    <img src="handshake.png" width="30" height="30"></img>
+                    Handshake
+                    <br></br>
+                  </TableCell>
                   <TableCell>
                     Handshake is the #1 way college students find jobs. Join
                     today to explore career options, find jobs and internships,
@@ -116,29 +68,114 @@ const CareerResourceList = props => {
                   </TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell>LinkedIn</TableCell>
+                  <TableCell>
+                    <img src="linked.png" width="30" height="30"></img>
+                    <br></br>
+                    LinkedIn
+                  </TableCell>
                   <TableCell>
                     LinkedIn is an American business and employment-oriented
                     service that operates via websites and mobile apps. It is
                     mainly used for professional networking, including employers
                     posting jobs and job seekers posting their CVs.
                   </TableCell>
-                  <TableCell>Test</TableCell>
+                  <TableCell>
+                    <a href="https://linkedin.com"> linkedin.com </a>
+                  </TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell>Indeed</TableCell>
+                  <TableCell>
+                    <img src="indeed.png" width="30" height="30"></img>
+                    <br></br>
+                    Indeed
+                  </TableCell>
                   <TableCell>
                     Indeed is an American worldwide employment-related search
                     engine for job listings.
                   </TableCell>
-                  <TableCell>Test</TableCell>
+                  <TableCell>
+                    <a href="https://indeed.com"> indeed.com </a>
+                  </TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell>Advisor Scheduler</TableCell>
+                  <TableCell>
+                    <img src="as.png" width="30" height="30"></img>
+                    <br></br>
+                    Advisor Scheduler
+                  </TableCell>
                   <TableCell>
                     At BetterUni, we have an easy to use advisor scheduler.
                   </TableCell>
-                  <TableCell>Test</TableCell>
+                  <TableCell>
+                    <Link to="/schedule">Schedule Page</Link>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
+            <h1>Upcoming Career Events</h1>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Event</TableCell>
+                  <TableCell>Date/Time</TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell>Link</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    Career Fair
+                    <br></br>
+                  </TableCell>
+                  <TableCell>
+                    October 5th 2020 <br></br> 12-4 PM
+                  </TableCell>
+                  <TableCell>
+                    Network with recruiters from over 50 different companies.
+                    Formal dress code required.
+                  </TableCell>
+                  <TableCell>
+                    <a href="https://joinhandshake.com"> joinhandshake.com </a>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Resume Review</TableCell>
+                  <TableCell>
+                    October 17th 2020 <br></br> 5-7 PM
+                  </TableCell>
+                  <TableCell>
+                    Meet up with a CST advisor to get a resume review in the
+                    Student Center!
+                  </TableCell>
+                  <TableCell>
+                    <a href="https://joinhandshake.com"> joinhandshake.com </a>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Penn Hackathon</TableCell>
+                  <TableCell>
+                    October 23-24 2020 <br></br> ALL DAY
+                  </TableCell>
+                  <TableCell>
+                    Penn University is having its annual hackathon on October
+                    23-24. Be sure to sign up on Handshake by October 7th if you
+                    wish to attend.
+                  </TableCell>
+                  <TableCell>
+                    <a href="https://joinhandshake.com"> joinhandshake.com </a>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Vanguard Networking Event</TableCell>
+                  <TableCell>
+                    October 26h 2020 <br></br> 12-3 PM
+                  </TableCell>
+                  <TableCell>
+                    Recruits from Vanguard will be visiting on campus in the
+                    SERC from 12-3 PM! Bring your resume!
+                  </TableCell>
+                  <TableCell>
+                    <a href="https://vanguard.com"> vanguard.com </a>
+                  </TableCell>
                 </TableRow>
               </TableHead>
             </Table>
