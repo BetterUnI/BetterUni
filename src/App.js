67388acfb9c673322457a2feaf1f5e52dc4481d2
Dillon Coffman <tug40394@temple.edu>
2020-04-09 18:17:46 -0400

    Removed unnecessary imports and comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// /* eslint-disable */
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { UserContext } from "./UserContext";
--- src/components/CometchatConference/CometchatConference.js
+++ src/components/CometchatConference/CometchatConference.js
@@ -1,14 +1,10 @@
 import React from "react";
 import CometChatUnified from "../../lib/CometChat/components/CometChatUnified";
-import CometChatConversationList from "../../lib/CometChat/components/CometChatConversationList";
-import CometChatMessageScreen from "../../lib/CometChat/components/CometChatMessageScreen";
 
 export default function CometchatConference() {
   return (
     <>
       <CometChatUnified />
-      {/* <CometChatConversationList /> */}
-      {/* <CometChatMessageScreen /> */}
     </>
   );
 }
