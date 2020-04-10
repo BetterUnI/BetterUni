1601893af27b8b6f054d4168f960392b23e5974f
tug67518 <tug67518@temple.edu>
2020-04-10 09:54:55 -0400

    Requested changes

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,9 +3,9 @@ import { UserContext } from "../../UserContext";
 import { makeStyles } from "@material-ui/styles";
 import HomeInfoList from "../../components/HomeInfoList/HomeInfoList";
 import SupervisorAccountOutlinedIcon from "@material-ui/icons/SupervisorAccountOutlined";
-import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import EventIcon from "@material-ui/icons/Event";
+import { Avatar as ListIcon } from "@material-ui/core";
 const useStyles = makeStyles(theme => ({
   content: {
     display: "inline-block",
@@ -92,51 +92,47 @@ export function HomePage() {
   const classes = useStyles();
   return (
     <>
-      <br></br>
       <div className={classes.page}>
         <center>
-          <br></br>
           <h1>Welcome to BetterUni</h1>
           <img src="images/bulogo.png" width="100" height="100"></img>
           <h2>We aim to provide you the tools, opportunities,</h2>
-          <h2> and resources to succeed at Temple Univeristy and beyond</h2>
+          <h2> and resources to succeed at Temple Univeristy and beyond!</h2>
         </center>
-        <br></br>
       </div>
-      <br></br>
       <div className={classes.content}>
         <HomeInfoList
           avatar={
-            <Avatar
+            <ListIcon
               variant="rounded"
               className={classes.officeAvatarBackgroundColor}
             >
               <SupervisorAccountOutlinedIcon fontSize="large" color="#FFFFFF" />
-            </Avatar>
+            </ListIcon>
           }
           listTitle="Advising Offices"
           homelists={offices}
         />
         <HomeInfoList
           avatar={
-            <Avatar
+            <ListIcon
               variant="rounded"
               className={classes.eventAvatarBackgroundColor}
             >
               <EventIcon fontSize="large" color="#FFFFFF" />
-            </Avatar>
+            </ListIcon>
           }
           listTitle="Career Events"
           homelists={event}
         />
         <HomeInfoList
           avatar={
-            <Avatar
+            <ListIcon
               variant="rounded"
               className={classes.meetingAvatarBackgroundColor}
             >
               <AccessTimeIcon fontSize="large" color="#FFFFFF" />
-            </Avatar>
+            </ListIcon>
           }
           listTitle="Upcoming Meetings"
           homelists={meeting}
