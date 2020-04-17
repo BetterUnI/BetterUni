dc4d9f6e99d1448dc340797d6fb6943ee4099bac
Noah Costello <noah@Noahs-MacBook-Pro.local>
2020-04-17 17:38:09 -0400

    new method to get event data is navigate to each event's unique url and pull it from there

--- amplify/backend/function/eventdata2/src/index.js
+++ amplify/backend/function/eventdata2/src/index.js
@@ -1,12 +1,41 @@
 // in index.js - make sure to `yarn add axios`in this directory - you can use `amplify invoke functionname` to test function running
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 exports.handler = async (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
 
-  const html = await axios.get("https://www.google.com");
-  const $ = await cheerio.load(html.statusText);
+  const html = await axios.get(
+    "https://events.temple.edu/calendar-grid?js-filter=true"
+  );
+  const $ = cheerio.load(html.data);
+  //console.log($.html());
+  let eventLinks = [];
+
+  $(".fullcalendar-event").each((i, elem) => {
+    if (i <= 5) {
+      eventLinks.push({
+        // title: $(elem)
+        //   .children("h3")
+        //   .text(),
+        link: $(elem) //gets link
+          .find(".fullcalendar-instance a")
+          .attr("href")
+      });
+    }
+  });
+
+  const singleEvent = await axios.get(
+    "https://events.temple.edu" + eventLinks[0].link
+  );
+  const a = cheerio.load(singleEvent.data);
+  console.log(a.html());
+  //eventLinks.forEach();
+  const title = a(".l-content-inner")
+    .children("h1")
+    .text();
+  console.log("TITLE: " + title);
 
   return {
     statusCode: 200,
