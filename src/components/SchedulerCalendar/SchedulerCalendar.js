04db51c8d96d0823d6ca9435e2b93348f394df98
Dillon Coffman <tug40394@temple.edu>
2020-04-16 20:58:22 -0400

    Set up Google event object for emailing a student a notification (a day before) and the advisor an invite (right away) to a new meeting event after the meeting event is created by the studentReplaced Google OAuth Client ID with environment variable to protect it

--- src/components/SchedulerCalendar/SchedulerCalendar.js
+++ src/components/SchedulerCalendar/SchedulerCalendar.js
@@ -1,34 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { gapi } from "gapi-script";
 
 var event = {
   summary: "Test Event",
-  location: "800 Howard St., San Francisco, CA 94103",
+  location: "1801 N. Broad Street, Philadelphia, PA 19122",
   description: "An event for testing purposes",
   start: {
-    dateTime: "2020-04-19T09:00:00-07:00",
+    dateTime: "2020-04-16T20:38:00+00:00",
     timeZone: "America/New_York"
   },
   end: {
-    dateTime: "2020-04-19T17:00:00-07:00",
+    dateTime: "2020-04-16T21:00:00+00:00",
     timeZone: "America/New_York"
+  },
+  // Attendees to send an email invite to (the advisor)
+  attendees: [
+    {
+      email: "studentemail.com",
+      displayName: "studentname"
+    },
+    {
+      email: "advisoremail.com",
+      displayName: "advisorname"
+    }
+  ],
+  // Sends email notification to organizer of event (student that scheduled meeting)
+  reminders: {
+    useDefault: false,
+    overrides: [
+      { method: "email", minutes: 1440 }, // 1 day before meeting
+      { method: "email", minutes: 120 } // 2 hours before meeting
+    ]
   }
 };
 
-function createGoogleEvent() {
-  gapi.client
+async function createGoogleEvent() {
+  return await gapi.client
     .load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
     .then(
       function() {
         console.log("GAPI client loaded for API");
         var request = gapi.client.calendar.events.insert({
           calendarId: "primary",
+          sendUpdates: "all", // sends email invite to attendees of event
           resource: event
         });
 
         request.execute(function(resp) {
-          console.log(resp);
-          // TODO: Store new event in a user's database
+          console.log("response: ", resp);
+          // TODO: Store new event as meeting in Meetings table for specific user
         });
       },
       function(err) {
@@ -38,6 +58,8 @@ function createGoogleEvent() {
 }
 
 export default function SchedulerCalendar() {
-  console.log(createGoogleEvent());
+  useEffect(() => {
+    createGoogleEvent();
+  }, []);
   return <div></div>;
 }
