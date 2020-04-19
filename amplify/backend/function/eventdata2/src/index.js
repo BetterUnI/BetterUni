79de038432beb0f0d6e4c9da9be330c1b6c03a1b
Noah Costello <noah@Noahs-MacBook-Pro.local>
2020-04-19 16:28:11 -0400

    Finished the implementation of the webscraping function

--- amplify/backend/function/eventdata2/src/index.js
+++ amplify/backend/function/eventdata2/src/index.js
@@ -6,60 +6,68 @@ exports.handler = async (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
 
   var today = new Date();
-
-  const html = await axios.get(
-    "https://events.temple.edu/calendar-grid?js-filter=true"
-  );
-  const $ = cheerio.load(html.data);
+  let url = "https://events.temple.edu/calendar-list?js-filter=true";
+  //ONLY GETS THE FIRST PAGE
+  let html = await axios.get(url);
+  let $ = cheerio.load(html.data);
   //console.log($.html());
   let eventData = [];
+  let pageLink = [];
 
-  $(".fullcalendar-event").each((i, elem) => {
-    if (i <= 3) {
-      eventData.push({
-        title: $(elem)
-          .children("h3")
-          .text(),
-        link:
-          "https://events.temple.edu" +
-          $(elem)
-            .find(".fullcalendar-instance a")
-            .attr("href"),
-        date: new Date(
-          $(elem)
+  const lastPage = $(".pager__item--last")
+    .find("a")
+    .attr("href");
+  console.log(lastPage);
+
+  let i = 1;
+
+  //Get the goods from one of the pages
+  try {
+    do {
+      $(".views-row").each((i, elem) => {
+        eventData.push({
+          title: $(elem)
+            .find(".views-field")
+            .find("a")
+            .children("h3")
+            .text(),
+          link:
+            "https://events.temple.edu" +
+            $(elem)
+              .find(".views-field")
+              .find("a")
+              .attr("href"),
+          date: new Date(
+            $(elem)
+              .find(".field--event-date-time")
+              .find(".date-display-single")
+              .attr("content")
+          ),
+          dateText: $(elem)
             .find(".date-display-single")
-            .attr("content")
-        ),
-        dateText: $(elem)
-          .find(".date-display-single")
-          .text(),
-        description: ""
+            .text(),
+          description: $(elem)
+            .find(".field--event-body")
+            .children("p")
+            .text()
+        });
       });
-    }
-  });
-
-  // try {
-  //   eventData.forEach(async element => {
-  //     console.log("ELEMENT.LINK: " + element.link + "\n");
-  //     const eventpage = await axios.get(element.link);
-  //     console.log("EVENT PAGE: " + eventpage + "\n");
-  //     const $$ = cheerio.load(eventpage.data);
-  //     console.log($$.html);
-
-  //     element.description = $$(".field--event-body")
-  //       .children("p")
-  //       .text();
-  //   });
-  // } catch (err) {
-  //   console.log(err);
-  // }
+      html = await axios.get(url, {
+        params: {
+          page: i++
+        }
+      });
+    } while (i <= 17);
+  } catch (err) {
+    console.log(err);
+  }
 
   /* If today - eventDate < 0 then that means the event 
     is in the future therefore add it to DB
   */
   //TODO: add DB code here
 
-  //sconsole.log(eventData);
+  console.log(eventData);
 
   return {
     statusCode: 200,
