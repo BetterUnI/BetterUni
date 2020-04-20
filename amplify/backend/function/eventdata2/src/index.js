b0251cc394e063ab97d7575d76ed488b623a370e
Noah Costello <noah@Noahs-MacBook-Pro.local>
2020-04-19 20:13:33 -0400

    Cleaned up variable and function names to improve readability and added comments

--- amplify/backend/function/eventdata2/src/index.js
+++ amplify/backend/function/eventdata2/src/index.js
@@ -1,81 +1,114 @@
-// in index.js - make sure to `yarn add axios`in this directory - you can use `amplify invoke functionname` to test function running
+/*
+If this function does not run ensure that you run 
+'npm install cheerio' and 'npm install axios' in
+'/betteruni/amplify/backend/function/eventdata2/src/'
+
+To run type: 'amplify mock function eventdata2'
+*/
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+/*
+Exports.handler is the main function triggered by a cron job.
+This job can be found in the parameters.json file
+*/
 exports.handler = async (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
 
-  var today = new Date();
+  // url to temple events page or page that you are attempting to scrape
   let url = "https://events.temple.edu/calendar-list?js-filter=true";
-  //ONLY GETS THE FIRST PAGE
-  let html = await axios.get(url);
-  let $ = cheerio.load(html.data);
-  //console.log($.html());
-  let eventData = [];
+  // The two lines below only get the first page of events
+  let html;
+  let htmlPageData;
 
-  const lastPage =
-    "https://events.temple.edu" +
-    $(".pager__item--last")
-      .find("a")
-      .attr("href");
-  let indexOfPageNum = lastPage.indexOf("page=");
-  let lastPageNum = lastPage.substring(indexOfPageNum + 5, lastPage.length);
+  try {
+    html = await axios.get(url);
+    htmlPageData = cheerio.load(html.data);
+  } catch (err) {
+    console.error("Error accessing " + url, err);
+    throw err;
+  }
+
+  let eventData = [];
+  let lastEventPageNumber = getLastPageNumber(htmlPageData);
 
   let i = 1;
+  /*
+    Each iteration of this loop will grab all the event data from
+    a single page and push it into the eventData array. It loops
+    until the last page which we find getLastPageNumber()
+  */
   try {
     do {
-      //Get the goods from one of the pages
       //eslint-disable-next-line no-loop-func
-      $(".views-row").each((i, elem) => {
+      htmlPageData(".views-row").each((i, elem) => {
         eventData.push({
-          title: $(elem)
+          title: htmlPageData(elem)
             .find(".views-field")
             .find("a")
             .children("h3")
             .text(),
           link:
             "https://events.temple.edu" +
-            $(elem)
+            htmlPageData(elem)
               .find(".views-field")
               .find("a")
               .attr("href"),
           date: new Date(
-            $(elem)
+            htmlPageData(elem)
               .find(".field--event-date-time")
               .find(".date-display-single")
               .attr("content")
           ),
-          dateText: $(elem)
+          dateText: htmlPageData(elem)
             .find(".date-display-single")
             .text(),
-          description: $(elem)
+          description: htmlPageData(elem)
             .find(".field--event-body")
             .children("p")
             .text()
         });
       });
+      /* We use the url and parameters to access new pages
+          and load them using cheerio
+      */
       try {
         html = await axios.get(url, {
           params: {
             page: i
           }
         });
-        $ = await cheerio.load(html.data);
+        htmlPageData = await cheerio.load(html.data);
       } catch (err) {
-        alert(err);
+        console.error("Error accessing" + url, err);
+        throw err;
       }
-    } while (i++ < lastPageNum);
+    } while (i++ < lastEventPageNumber);
   } catch (err) {
-    alert(err);
+    console.error("Error in main loop:", err);
+    throw err;
   }
 
-  /* If today - eventDate < 0 then that means the event 
-    is in the future therefore add it to DB
-  */
-  //TODO: add DB code here
+  // TODO: add DB code here
+  console.log(eventData[0]);
 
   return {
     statusCode: 200,
     body: JSON.stringify(html.statusText)
   };
+};
+
+/*
+This function will take the cheerio object and find the 
+url of the last event page. This url is then scraped and
+returns for the page number of the last page
+*/
+const getLastPageNumber = htmlPageData => {
+  const lastEventPage =
+    "https://events.temple.edu" +
+    htmlPageData(".pager__item--last")
+      .find("a")
+      .attr("href");
+  let indexOfPageNumber = lastEventPage.indexOf("page=");
+  return lastEventPage.substring(indexOfPageNumber + 5, lastEventPage.length);
 };
