e5bda4bdf325a3ba0458e1a358b420fa82946cc7
Dillon Coffman <tug40394@temple.edu>
2020-04-21 13:18:06 -0400

    Added code to create a GraphQL meeting after the Google Calendar meeting is created; this sends the actual studentUser and advisorUser an invite and notification email

--- src/components/SchedulerCalendar/SchedulerCalendar.js
+++ src/components/SchedulerCalendar/SchedulerCalendar.js
@@ -1,52 +1,146 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "../../UserContext";
 import { gapi } from "gapi-script";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import { API, graphqlOperation } from "aws-amplify";
+import { createMeeting as CreateMeeting } from "../../graphql/mutations";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-async function createGoogleEvent(startTime, endTime, title) {
+// async function createGoogleEvent(studentUser, startTime, endTime, title) {
+//   return await gapi.client
+//     .load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
+//     .then(
+//       function() {
+//         console.log("GAPI client loaded for Google Calendar API");
+//         var request = gapi.client.calendar.events
+//           .insert({
+//             calendarId: "primary",
+//             sendUpdates: "all", // sends email invite to attendees of event
+//             resource: {
+//               summary: title,
+//               location: "1801 N. Broad Street, Philadelphia, PA 19122",
+//               description: "An event for testing purposes",
+//               start: {
+//                 dateTime: startTime,
+//                 timeZone: "America/New_York"
+//               },
+//               end: {
+//                 dateTime: endTime,
+//                 timeZone: "America/New_York"
+//               },
+//               // Attendees to send an email invite to (the advisor)
+//               attendees: [
+//                 {
+//                   email: studentUser.email,
+//                   displayName: `${studentUser.firstName}+${studentUser.lastName}`
+//                 },
+//                 {
+//                   email: "advisoremail@gmail.com",
+//                   displayName: "advisorname"
+//                 }
+//               ],
+//               // Sends email notification to organizer of event (student that scheduled meeting)
+//               reminders: {
+//                 useDefault: false,
+//                 overrides: [
+//                   { method: "email", minutes: 1440 }, // 1 day before meeting
+//                   { method: "email", minutes: 120 } // 2 hours before meeting
+//                 ]
+//               }
+//             }
+//           })
+//           .then(() => {
+//             // GraphQL code to save meeting for users
+//             // TODO: Insert GraphQL API code in order to push event to Meetings table
+//           });
+
+//         request.execute(function(resp) {
+//           console.log("Google Calendar response: ", resp);
+//         });
+//       },
+//       function(err) {
+//         console.error("Error loading GAPI client for API", err);
+//       }
+//     );
+// }
+
+// TODO once ClickedAdvisorContext is created: const advisorUser = useContext(ClickedAdvisorContext);
+async function createMeeting(
+  studentUser,
+  advisorUser,
+  startTime,
+  endTime,
+  title
+) {
   return await gapi.client
     .load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
     .then(
       function() {
         console.log("GAPI client loaded for API");
-        var request = gapi.client.calendar.events.insert({
-          calendarId: "primary",
-          sendUpdates: "all", // sends email invite to attendees of event
-          resource: {
-            summary: title,
-            location: "1801 N. Broad Street, Philadelphia, PA 19122",
-            description: "An event for testing purposes",
-            start: {
-              dateTime: startTime,
-              timeZone: "America/New_York"
-            },
-            end: {
-              dateTime: endTime,
-              timeZone: "America/New_York"
-            },
-            // Attendees to send an email invite to (the advisor)
-            attendees: [
-              {
-                email: "studentemail@gmails.com",
-                displayName: "studentname"
+        var request = gapi.client.calendar.events
+          .insert({
+            calendarId: "primary",
+            sendUpdates: "all", // sends email invite to attendees of event
+            resource: {
+              summary: title,
+              location: "1801 N. Broad Street, Philadelphia, PA 19122",
+              description: "An event for testing purposes",
+              start: {
+                dateTime: startTime,
+                timeZone: "America/New_York"
               },
-              {
-                email: "advisoremail@gmail.com",
-                displayName: "advisorname"
+              end: {
+                dateTime: endTime,
+                timeZone: "America/New_York"
+              },
+              // Attendees to send an email invite to (the advisor)
+              attendees: [
+                {
+                  email: studentUser.email,
+                  displayName: `${studentUser.firstName}+${studentUser.lastName}`
+                },
+                {
+                  email: advisorUser.email,
+                  displayName: `${advisorUser.firstName}+${advisorUser.lastName}`
+                }
+              ],
+              // Sends email notification to organizer of event (student that scheduled meeting)
+              reminders: {
+                useDefault: false,
+                overrides: [
+                  { method: "email", minutes: 1440 }, // 1 day before meeting
+                  { method: "email", minutes: 120 } // 2 hours before meeting
+                ]
               }
-            ],
-            // Sends email notification to organizer of event (student that scheduled meeting)
-            reminders: {
-              useDefault: false,
-              overrides: [
-                { method: "email", minutes: 1440 }, // 1 day before meeting
-                { method: "email", minutes: 120 } // 2 hours before meeting
-              ]
             }
-          }
-        });
+          })
+          .then(async () => {
+            // GraphQL code to save meeting for users
+            // TODO: Insert GraphQL API code in order to push event to Meetings table
+            try {
+              const newMeeting = await API.graphql(
+                graphqlOperation(CreateMeeting, {
+                  input: {
+                    // date: date
+                    startTime: startTime,
+                    endTime: endTime,
+                    reasonForMeeting: title,
+                    meetingUserId: studentUser.id,
+                    meetingAdvisorUserId: advisorUser.id
+                  }
+                })
+              );
+              console.log(
+                "GraphQL newMeeting created in Meeting DynamoDB table: ",
+                newMeeting
+              );
+              return newMeeting;
+            } catch (err) {
+              console.log("There was an error creating a meeting: ", err);
+            }
+          });
 
         request.execute(function(resp) {
           console.log("response: ", resp);
@@ -62,9 +156,12 @@ async function createGoogleEvent(startTime, endTime, title) {
 export default function SchedulerCalendar() {
   const [events, setEvents] = useState([]);
   const localizer = momentLocalizer(moment);
+  const studentUser = useContext(UserContext);
+  // TODO once ClickedAdvisorContext is created: const advisorUser = useContext(ClickedAdvisorContext);
+  console.log("SchedulerCalendar user: ", studentUser);
 
   const handleSelect = ({ start, end }) => {
-    const title = window.prompt("New Event name");
+    const title = window.prompt("Reason for Meeting");
     if (title)
       setEvents(
         events.concat({
@@ -81,9 +178,9 @@ export default function SchedulerCalendar() {
     console.log("End time is ", formattedEndTime);
 
     // Creates Google Calendar event
-    createGoogleEvent(formattedStartTime, formattedEndTime, title);
+    // OLD createGoogleEvent(studentUser, formattedStartTime, formattedEndTime, title);
 
-    // TODO: Insert GraphQL API code in order to push event to Meetings table
+    // NEW once ClickedAdvisorContext is created: createMeeting(studentUser, advisorUser, formattedStartTime, formattedEndTime, title);
   };
 
   return (
