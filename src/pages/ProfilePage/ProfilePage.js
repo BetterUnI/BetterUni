53abdd26f1f04684028ab265308b26a879042633
Dillon Coffman <tug40394@temple.edu>
2020-04-24 13:46:00 -0400

    Touched up profile page

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -4,15 +4,12 @@ import { UserContext } from "../../UserContext";
 import MeetingsCalendar from "../../components/MeetingsCalendar/MeetingsCalendar";
 
 export function ProfilePage() {
-  // eslint-disable-next-line no-unused-vars
-  const user = useContext(UserContext); // will use to display user's current meetings data with user.meetings array
-  // console.log("user on profile page: ", user);
+  const user = useContext(UserContext);
   const userMeetings = user.meetings.items;
 
   return (
     <>
-      <h1>Profile page</h1>
-      <p>This is the profile page</p>
+      <h1>Meeting Calendar</h1>
       <MeetingsCalendar meetings={userMeetings} />
     </>
   );
